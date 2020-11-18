4cbf70b9fd960ff2c4980465f60e31412b78b779
Mykola Myronenko <nicholas.mironenko@gmail.com>
2020-11-18 19:18:28 +0200

    #7 Blocks and templates: Added a CMS block with notice to the page before all other blocks

--- app/etc/config.php
+++ app/etc/config.php
@@ -327,6 +327,7 @@ return [
         'Klarna_Kp' => 1,
         'Klarna_Onsitemessaging' => 1,
         'Klarna_KpGraphQl' => 1,
+        'MykolaM_Cms' => 1,
         'MykolaM_LayoutDebug' => 1,
         'PayPal_Braintree' => 1,
         'PayPal_BraintreeGraphQl' => 1,
